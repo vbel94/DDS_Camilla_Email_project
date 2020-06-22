7290c3dd2855b3c3bd171a2b75d3c9cca9a5e3a1
vbel94 <42897291+vbel94@users.noreply.github.com>
2020-06-22 20:26:50 +0300

    last commit
    last commit

--- camellia/src/camellia/Main.py
+++ camellia/src/camellia/Main.py
@@ -1,6 +1,6 @@
 import camellia
 plain = b"dddd is a text."
-c1 = camellia.CamelliaCipher(key=b'16 byte long key', IV=b'16 byte iv. abcd', mode=camellia.MODE_CBC)
+c1 = camellia.CamelliaCipher(key=b'32 byte long key32 byte long key', IV=b'16 byte iv. abcd', mode=camellia.MODE_CBC)
 encrypted = c1.encrypt(plain)
 c2 = camellia.CamelliaCipher(key=b'16 byte long keyaaa', IV=b'16 byte iv. abcd', mode=camellia.MODE_CBC)
 decrypted = c2.decrypt(encrypted)
